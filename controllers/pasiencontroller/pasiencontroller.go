4f1d8a8527e0f4cdcde6a32d746360338c52ae89
Archie-study <archiestudy12@gmail.com>
2024-02-13 12:26:56 +0700

    Code Update and Delete Method

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -3,11 +3,14 @@ package pasiencontroller
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
 	"github.com/Archie-study/go-crud/entities"
+	"github.com/Archie-study/go-crud/libraries"
 	"github.com/Archie-study/go-crud/models"
 )
 
+var validation = libraries.NewValidation()
 var pasienModel = models.NewPasienModel()
 
 func Index(response http.ResponseWriter, request *http.Request) {
@@ -48,9 +51,16 @@ func Add(response http.ResponseWriter, request *http.Request) {
 		pasien.Alamat = request.Form.Get("alamat")
 		pasien.NoHp = request.Form.Get("no_hp")
 
-		pasienModel.Create(pasien)
-		data := map[string]interface{}{
-			"pesan": "Data pasien berhasil disimpan",
+		var data = make(map[string]interface{})
+
+		vErrors := validation.Struct(pasien)
+
+		if vErrors != nil {
+			data["pasien"] = pasien
+			data["validation"] = vErrors
+		} else {
+			data["pesan"] = "Data pasien berhasil disimpan"
+			pasienModel.Create(pasien)
 		}
 
 		temp, _ := template.ParseFiles("views/pasien/add.html")
@@ -60,5 +70,64 @@ func Add(response http.ResponseWriter, request *http.Request) {
 
 }
 
-func Edit(response http.ResponseWriter, request *http.Request)   {}
-func Delete(response http.ResponseWriter, request *http.Request) {}
+func Edit(response http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+
+		queryString := request.URL.Query()
+		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+		var pasien entities.Pasien
+		pasienModel.Find(id, &pasien)
+
+		data := map[string]interface{}{
+			"pasien": pasien,
+		}
+
+		temp, err := template.ParseFiles("views/pasien/edit.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, data)
+	} else if request.Method == http.MethodPost {
+
+		request.ParseForm()
+
+		var pasien entities.Pasien
+
+		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHp = request.Form.Get("no_hp")
+
+		var data = make(map[string]interface{})
+
+		vErrors := validation.Struct(pasien)
+
+		if vErrors != nil {
+			data["pasien"] = pasien
+			data["validation"] = vErrors
+		} else {
+			data["pesan"] = "Data pasien berhasil diperbarui"
+			pasienModel.Update(pasien)
+		}
+
+		temp, _ := template.ParseFiles("views/pasien/edit.html")
+		temp.Execute(response, data)
+
+	}
+}
+
+func Delete(response http.ResponseWriter, request *http.Request) {
+
+	queryString := request.URL.Query()
+	id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+	pasienModel.Delete(id)
+
+	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
+
+}
--- models/pasienmodel.go
+++ models/pasienmodel.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/Archie-study/go-crud/config"
 	"github.com/Archie-study/go-crud/entities"
@@ -51,6 +52,9 @@ func (p *PasienModel) FindAll() ([]entities.Pasien, error) {
 			pasien.JenisKelamin = "Perempuan"
 		}
 
+		tgl_lahir, _ := time.Parse("2006-01-02", pasien.TanggalLahir)
+		pasien.TanggalLahir = tgl_lahir.Format("02-01-2006")
+
 		dataPasien = append(dataPasien, pasien)
 	}
 
@@ -70,4 +74,35 @@ func (p *PasienModel) Create(pasien entities.Pasien) bool {
 	lastInsertId, _ := result.LastInsertId()
 
 	return lastInsertId > 0
+}
+
+func (p *PasienModel) Find(id int64, pasien *entities.Pasien) error {
+
+	return p.conn.QueryRow("select * from pasien where id = ?", id).Scan(
+		&pasien.Id,
+		&pasien.NamaLengkap,
+		&pasien.NIK,
+		&pasien.JenisKelamin,
+		&pasien.TempatLahir,
+		&pasien.TanggalLahir,
+		&pasien.Alamat,
+		&pasien.NoHp,
+	)
+}
+
+func (p *PasienModel) Update(pasien entities.Pasien) error {
+
+	_, err := p.conn.Exec(
+		"update pasien set nama_lengkap = ?, nik = ?, jenis_kelamin = ?, tempat_lahir = ?, tanggal_lahir = ?, alamat = ?, no_hp = ? where id = ?",
+		pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHp, pasien.Id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PasienModel) Delete(id int64) {
+	p.conn.Exec("delete from pasien where id = ?", id)
 }
